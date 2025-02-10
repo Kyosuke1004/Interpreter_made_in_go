1cf8adfae945ff412d21deaae122ac1b7d232545
Kyosuke1004 <kyosukeippoku297@icloud.com>
2025-02-10 09:48:40 +0900

    書籍に書いてあるやつ完成版

---
+++ token/teken.go
@@ -0,0 +1,73 @@
+// token/token.go
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT  = "IDENT" // add, foobar, x,y,　...
+	INT    = "INT"   // 1343456
+	STRING = "STRING"
+
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// デリミタ
+	COMMA     = ","
+	COLON     = ":"
+	SEMICOLON = ";"
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	MACRO    = "MACRO"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"macro":  MACRO,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
